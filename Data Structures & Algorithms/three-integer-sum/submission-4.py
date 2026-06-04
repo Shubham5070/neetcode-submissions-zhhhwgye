c331dbf45ebdfae1bd5cddd1644bb95cadd9e686
Shubham5070 <103039287+Shubham5070@users.noreply.github.com>
2026-06-04 12:11:36 +0530

    Update: three-integer-sum - submission-4

--- Data Structures & Algorithms/three-integer-sum/submission-4.py
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -1,35 +1,16 @@
 class Solution:
-    def threeSum(self, nums):
-        s = sorted(nums)
-        res = []
-        
-        for i in range(len(s)):
-            if i > 0 and s[i] == s[i-1]:
-                continue
-            
-            tar = -s[i]
-            left = i + 1
-            right = len(s) - 1
-            
-            while left < right:
-                total = s[left] + s[right]
-                
-                if total == tar:
-                    res.append([s[i], s[left], s[right]])
-                    
-                    # ✅ skip duplicates AFTER match
-                    while left < right and s[left] == s[left+1]:
-                        left += 1
-                    while left < right and s[right] == s[right-1]:
-                        right -= 1
-                    
-                    left += 1
-                    right -= 1
-                
-                elif total < tar:
-                    left += 1
-                
-                else:
-                    right -= 1
-        
-        return res
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+
+        for i in range(len(nums)):
+            seen = set()
+
+            for j in range(i + 1, len(nums)):
+                third = -(nums[i] + nums[j])
+
+                if third in seen:
+                    res.add(tuple(sorted([nums[i], nums[j], third])))
+
+                seen.add(nums[j])
+
+        return [list(t) for t in res]
